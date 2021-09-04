c33bae7939f3683b1839fdbc0cb488ea05dd8622
Bryan Pérez <bryanper@Bryans-MacBook-Air.local>
2021-09-04 16:08:20 -0500

    feat: implementation of redux in typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
-import "./styles/main.scss"
+import './styles/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './routes/App';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
